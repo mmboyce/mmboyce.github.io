45d064bb866a95c1fb3331cc709a7660bcef2415
mmboyce <mathieumimmsboyce@gmail.com>
2019-09-26 17:56:50 -0400

    updated interests

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -6,8 +6,8 @@ class Interests extends React.Component {
             <div id="interests" class="section">
                 <h1>Interests</h1>
                 <div id='interests-box'>
-                    <p>At the University of Delaware I have been involved with both the <a href="https://sites.udel.edu/nsbe/">National Society of Black Engineers</a> and <a href="http://www.sigphipsi.com/aboutAboutUs.html">The Premier Engineering Fraternity, Sigma Phi Delta.</a> I was elected <b>President</b> of our chapter in November of 2018. Prior to my presidency, I was the <b>PR Chair,</b> both running our social media and designing fraternity apparel.</p>
-                    <p>Outside of my roles in on-campus organizations I spend a lot of time being active. The University of Delaware's Bouldering Gym is a place I have spent a lot of free time. If I don't feel like climbing the rock wall at the gym, I typically spend time on the turf playing soccer with my friends.</p>
+                    <p>During my time at the University of Delaware I was involved with both the <a href="https://sites.udel.edu/nsbe/">National Society of Black Engineers</a> and <a href="http://www.sigphipsi.com/aboutAboutUs.html">The Premier Engineering Fraternity, Sigma Phi Delta.</a> I was elected <b>President</b> of our chapter in November of 2018. Prior to my presidency, I was the <b>PR Chair,</b> both running our social media and designing fraternity apparel.</p>
+                    <p>Presently, I spend a lot of time being active. I like to go for runs, and lift, but whem I'm not doing that I love spending time with my friends playing pickup soccer games, dancing, or playing video games. I used to rock climb pretty frequently too, so now I'm hoping to find a good bouldering gym near me!</p>
                 </div>
             </div>
         );
