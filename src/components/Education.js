84b640f7902491b39f353e06691b258b6a087778
mmboyce <mathieumimmsboyce@gmail.com>
2019-09-26 17:46:51 -0400

    Updated education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,11 +5,17 @@ class Education extends React.Component {
         return(
             <div id="education" class="section">
                 <h1>Education</h1>
+                <div id="umuc" class="school">
+                    <h2>University of Maryland Global Campus</h2>
+                    <h3>Bachelor of Science</h3>
+                    <p>Computer Science</p>
+                    <h5>September 2019 - July 2020 <i>(Expected)</i></h5>
+                </div>
                 <div id="ud" class="school">
                     <h2>University of Delaware</h2>
                     <h3>Bachelor of Arts</h3>
                     <p>Computer Science</p>
-                    <h5>August 2016 - Present</h5>
+                    <h5>August 2016 - September 2019 <i>(Transferred from)</i></h5>
                 </div>
             </div>
         );
