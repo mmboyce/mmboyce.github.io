338d5adbabf5d3f9f36484558d38a55b37b7ee47
mmboyce <mathieumimmsboyce@gmail.com>
2020-03-25 17:28:16 -0400

    Remove sidebar>

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import About from './components/About';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
-import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 import 'normalize.css';
 
 function App() {
   return (
     <div id="App">
-      <Sidebar
-        pageWrapId="page-wrap"
-        outerContainerId="App"
-      />
 
       <div id="page-wrap">
         <About />
